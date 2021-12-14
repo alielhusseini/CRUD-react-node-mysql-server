b999ea23aacffefa5f5c984f5de0f8dbc93f5c77
Ali El Husseini <ali.elhusseini@ctsjounieh.com>
2021-12-14 09:44:39 +0200

    get post delete operations

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// imports
 const express = require('express')
 const mysql = require('mysql')
 const cors = require('cors')
 
+// setup
 const db = mysql.createConnection({
     user: 'root',
     host: 'localhost',
@@ -18,6 +20,7 @@ app.listen(5000, () => console.log('server running'))
 app.use(cors())
 app.use(express.json())
 
+// routes
 app.post('/create', (req, res) => {
     const { name, age, country, position, wage } = req.body
 
@@ -25,8 +28,28 @@ app.post('/create', (req, res) => {
         "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)", [name, age, country, position, wage],
         (err, result) => {
             if (err) console.log(err)
-            else console.log('success')
-            res.end()
+            else res.send('Values inserted')
+        }
+    )
+});
+
+app.get('/employees', (req, res) => {
+    db.query(
+        "SELECT * FROM employees",
+        (err, result) => {
+            if (err) console.log(err)
+            else res.send(result)
+        }
+    )
+});
+
+app.delete('/employees/:id', (req, res) => {
+    const { id } = req.params;
+    db.query(
+        `DELETE FROM employees WHERE id=${ id }`,
+        (err, result) => {
+            if (err) console.log(err)
+            else res.send('Deleted successfully')
         }
     )
 });
